ba812952905562d880c784bb1c01a6a6df01a3cf
SUJAN DAS <dassujan.005@gmail.com>
2023-05-24 23:24:02 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import Container from '@mui/material/Container';
+import Header from './Component/Header/header.jsx';
+import Definitions from './Component/Defination/definitions.jsx';
 
 function App() {
+
+  const [meanings, setMeanings] = useState([]);
+  const [input, setInput] = useState("");
+
+  const dictionaryApiData = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
+      );
+      setMeanings(data.data)
+      console.log(meanings[0].phonetics)
+
+    } catch {
+
+    }
+  }
+
+  useEffect(
+    () => {
+      dictionaryApiData()
+    }, [input]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: 'black', color: '#fff', height: '100vh' }}>
+      <Container maxWidth="md" style={{ diaplay: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <Header input={input} setInput={setInput} />
+        <Definitions meanings={meanings} word={input} />
+      </Container>
     </div>
+
   );
 }
 
