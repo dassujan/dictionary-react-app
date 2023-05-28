e811c29b31ce9d40dd179f9a62c80994ae50319b
SUJAN DAS <dassujan.005@gmail.com>
2023-05-28 16:07:52 +0530

    Updates

---
+++ precache-manifest.3535bc92770ee4514acc864608467235.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b7b64cb48829ab8e7f3",
+    "url": "/static/css/main.dd897f60.chunk.css"
+  },
+  {
+    "revision": "4b7b64cb48829ab8e7f3",
+    "url": "/static/js/main.4b7b64cb.chunk.js"
+  },
+  {
+    "revision": "86f4eb415309d4d2f407",
+    "url": "/static/js/1.86f4eb41.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08a390e669832ccaa99632910dff1e4c",
+    "url": "/index.html"
+  }
+];
